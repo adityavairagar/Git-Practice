099637253be4cb45a7e749b1278b9f82deddf09f
aditya <adityavairagar5@gmail.com>
2023-01-19 15:56:48 +0530

    new class added hashmap

--- src/com/velocity/GitPractice/ArrayListDemo.java
+++ src/com/velocity/GitPractice/ArrayListDemo.java
@@ -1,13 +1,10 @@
 package com.velocity.GitPractice;
 
-import java.util.ArrayList;
+//import java.util.ArrayList;
 
 public class ArrayListDemo {
  public static void main(String[] args) {
-	ArrayList<Integer> list=new ArrayList<>();
-	list.add(20);
-	list.add(63);
-	list.add(52);
-System.out.println(list);	
+	
+System.out.println("hello");	
  }
 }
---
+++ src/com/velocity/GitPractice/HashMapDemo.java
@@ -0,0 +1,15 @@
+package com.velocity.GitPractice;
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+public static void main(String[] args) {
+  
+	HashMap<Integer, String> map=new HashMap<Integer, String>();
+	 map.put(10, "Praveen");
+	 map.put(102, "Naveen");
+	 map.put(105, "sujeet");
+	System.out.println(map);
+
+}
+}
