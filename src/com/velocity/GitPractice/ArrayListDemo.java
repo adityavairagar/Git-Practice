323e6058d5a182aa728761f27f8708c9002d5021
aditya <adityavairagar5@gmail.com>
2023-01-19 16:38:06 +0530

    Arraylist class implementation

--- src/com/velocity/GitPractice/ArrayListDemo.java
+++ src/com/velocity/GitPractice/ArrayListDemo.java
@@ -1,10 +1,17 @@
 package com.velocity.GitPractice;
 
+import java.util.LinkedList;
+import java.util.List;
+
 //import java.util.ArrayList;
 
 public class ArrayListDemo {
  public static void main(String[] args) {
 	
-System.out.println("hello");	
+   List<String> list=new LinkedList<String>();
+   list.add("jayesh");
+   list.add("jaidiip");
+   list.add("pradeep");
+   System.out.println(list);
  }
 }
--- src/com/velocity/GitPractice/HashMapDemo.java
+++ src/com/velocity/GitPractice/HashMapDemo.java
@@ -3,8 +3,7 @@ package com.velocity.GitPractice;
 import java.util.HashMap;
 
 public class HashMapDemo {
-public static void main(String[] args) {
-  
+public static void main(String[] args) {  
 	HashMap<Integer, String> map=new HashMap<Integer, String>();
 	 map.put(10, "Praveen");
 	 map.put(102, "Naveen");
