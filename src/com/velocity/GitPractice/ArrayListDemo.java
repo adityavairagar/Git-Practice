cb15234e05bea36ced90d88d235afa55494af097
aditya <adityavairagar5@gmail.com>
2023-01-19 15:19:19 +0530

    Git practice 1

---
+++ src/com/velocity/GitPractice/ArrayListDemo.java
@@ -0,0 +1,13 @@
+package com.velocity.GitPractice;
+
+import java.util.ArrayList;
+
+public class ArrayListDemo {
+ public static void main(String[] args) {
+	ArrayList<Integer> list=new ArrayList<>();
+	list.add(20);
+	list.add(63);
+	list.add(52);
+System.out.println(list);	
+ }
+}
